ad945894cf21552ec24a50af504035244f80900e
Moe1711 <71646427+Moe1711@users.noreply.github.com>
2021-10-01 13:25:54 +0000

    oooof

--- Main.java
+++ Main.java
@@ -5,17 +5,22 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-
+//main question
     System.out.print("Which would you like to choose.\n A) Adittion table\n B) Multiplication Table\n C) Logic table for boolean logic’s AND gate\n D) Logic table for boolean logic’s OR gate\n E) Pascal's triangle\n");
 
     String userChoice = input.nextLine();
-
-    for(int i = 1; i<=10; i++) {
+    
+//addition table    
+    for(int i = 0; i<=10; i++) {
       if (userChoice.equalsIgnoreCase("a")) {
-        System.out.print(i);
+        System.out.print( i + " \t");
+          for(int j = 1; j<10; j++){
+           System.out.print( i + "\t");
+          }
+        System.out.print("\n");//spacing
       }
     }
-
+   
 
   }
 }
