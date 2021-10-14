1be6241ce2c4d9a33996159c9b9736546a3d1526
Moe1711 <71646427+Moe1711@users.noreply.github.com>
2021-10-14 14:54:57 +0000

    maybe stuff

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 import java.util.Scanner;
 import java.util.*;
+import java.io.*;
 
 
 class Main {
+  public static final String TEXT_RESET = "\u001B[0m";
+  public static final String RED = "\u001B[31m";
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+
 //main question
-    System.out.print("Which would you like to choose.\n A) Addition table\n B) Multiplication Table\n C) Logic table for boolean logic’s AND gate\n D) Logic table for boolean logic’s OR gate\n E) Pascal's triangle\n");
+    System.out.print("Which would you like to choose.\n A) Addition Table\n B) Multiplication Table\n C) Logic table for boolean logic’s AND gate\n D) Logic table for boolean logic’s OR gate\n E) Pascal's triangle\n F) Remainder Table\n");
 
     String userChoice = input.nextLine();
    
@@ -19,44 +23,77 @@ class Main {
         System.out.print("\n");//spacing
       }
     }
+
 //Multiplication table
-    else if (userChoice.equalsIgnoreCase("b")) {
+    double square;
+    if (userChoice.equalsIgnoreCase("b")) {
       int [][] twoArray = new int [11][11]; 
       for(int i = 1; i<11; i++){
         for(int o = 1; o<11; o++)
         {
           twoArray[i][o] = i * o;// math part
-          System.out.print(twoArray[i][o] + "\t");
+          square = Math.sqrt(twoArray[i][o]);
+          square = (Math.floor(square)*Math.floor(square));
+          if (square == twoArray[i][o]){
+            System.out.print(RED);
+          }
+          
+          System.out.print(twoArray[i][o] + "\t" +TEXT_RESET);
+          
         }
         System.out.println();// so it's a table not a long line
       }
     }
+
 // logic or gate
-    boolean question1, question2, ans;
+    int n;//declaring stuff
+    boolean [] rowA = new boolean [4];
+    boolean [] rowB = new boolean [4];
+    boolean ans;
     if (userChoice.equalsIgnoreCase("d")){
+      n = 0;
       System.out.println("Condition 1\tCondition2\tOR");
       for(int i =0; i<=1; i++){
         for(int o=0; o<=1; o++){
-          question1 = (i== 1);
-          question2 = (o== 1);
-          ans = (question1 == true || question2 == true);
-          System.out.println(question1+ "\t\t" + question2+ "\t\t" + ans);
+          rowA [n] = (i== 1);
+          rowB [n] = (o== 1);
+          ans = (rowA[n] == true || rowB[n] == true);//or gate
+          System.out.println(rowA [n]+ "\t\t" + rowB[n]+ "\t\t" + ans);
         }
       }
     }   
+
     //AND gate 
-    boolean quest1, quest2, answer;
+    boolean answer;
     if (userChoice.equalsIgnoreCase("c")){
+      n =0;
       System.out.println("Condition 1\tCondition2\tAND");
       for(int i =0; i<=1; i++){
         for(int o=0; o<=1; o++){
-          quest1 = (i== 1);
-          quest2 = (o== 1);
-          answer = (quest1 == true && quest2 == true);
-          System.out.println(quest1+ "\t\t" + quest2+ "\t\t" + answer);
+          rowA[n] = (i== 1);
+          rowB[n] = (o== 1);
+          answer = (rowA[n] == true && rowB[n] == true);//and gate
+          System.out.println(rowA[n]+ "\t\t" + rowB[n]+ "\t\t" + answer);
         }
       }
-    }    
+    }  
+
+    //remainder table
+    
+    if (userChoice.equalsIgnoreCase("f")) {
+      for(int i=1; i<11; i++){
+       System.out.print(i + "\t"); 
+      }
+      System.out.println();
+      for(int i = 1; i<11; i++){
+        System.out.print(i);
+        for(int o = 1; o<11; o++){
+          System.out.print( i % o + "\t");
+          
+        }
+        System.out.println();// so it's a table not a long line
+      }
+    }  
 
   }
 }
